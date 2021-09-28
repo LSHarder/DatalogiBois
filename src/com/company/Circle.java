0f7948cb52544b9c357483f2fd5f3694895f5d12
LSHarder <90760113+LSHarder@users.noreply.github.com>
2021-09-28 16:19:06 +0200

    lavet en set function i Circle

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,5 +1,10 @@
 package com.company;
 
 public class Circle extends Shape {
-
+    float radius;
+    public void set(float x, float y, float radius) {
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    Circle c = new Circle();
+	    c.set(2,3,5);
 	    System.out.println(c.returnCenter());
     }
 }

cc225e8d2fe77885481198e5a8940ad33675ea31
Loei <lmi@ruc.dk>
2021-09-30 14:12:52 +0200

    Changes in main and triangle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,13 @@ public class Main {
 		System.out.println(r.area());
 		System.out.println(r.circumference());
 		System.out.println(r.overlapPoint(1,2));
+		Triangle t = new Triangle();
+		t.set(4,6,10,20 );
+		System.out.println(t.returnCenter());
+		System.out.println(t.area());
+		System.out.println(t.circumference());
+		System.out.println(t.overlapPoint(1,2));
+
     }
 }
 
--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.lang.Math;
 
 public class Triangle extends Shape {
@@ -11,17 +12,31 @@ public class Triangle extends Shape {
         this.x = x;
         this.y = y;
     }
+
     public double area() {
         return (this.height * this.width) / 2;
     }
-    public double circumference(){
-        return Math.sqrt(Math.pow((this.width/2),2)+Math.pow(this.height,2))*2+this.width;
+
+    public double circumference() {
+        return Math.sqrt(Math.pow((this.width / 2), 2) + Math.pow(this.height, 2)) * 2 + this.width;
     }
 
-    public boolean overlapPoint (float x, float y) {
+    public boolean overlapPoint(float x, float y) {
+        float x1 =this.x;
+        float y1 = this.y;
+        float x2 = this.x + this.width;
+        float y2 = this.y;
+        float x3 = this.x;
+        float y3 = this.y + this.height;
+
+        float a1 = (x1*(y2 - y) + x2*(y - y1) + x*(y1 - y2)) / 2;
+        float a2 = (x1*(y - y3) + x*(y3 - y1) + x3*(y1 - y)) / 2;
+        float a3 = (x*(y2 - y3) + x2*(y3 - y) + x3 * (y - y2)) / 2;
+
+
+        return this.area() == a1 + a2 + a3 && a1 >= 0 && a2 >= 0 && a3 >= 0;
+
     }
-
-
 
 
 }
